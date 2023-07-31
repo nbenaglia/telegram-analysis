fe76e8340fcd1ac4c09c8a37e413558fdc9ba2a1
Nicola Benaglia <nico.benaz@gmail.com>
2023-07-31 09:10:20 +0200

    first commit

---
+++ DeletedMessages.py
@@ -0,0 +1,81 @@
+import configparser
+import json
+import time
+from datetime import date, datetime
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+    PeerChannel
+)
+
+# some functions to parse json date
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        if isinstance(o, bytes):
+            return list(o)
+
+        return json.JSONEncoder.default(self, o)
+
+# Reading Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Setting configuration values
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+
+api_hash = str(api_hash)
+
+phone = config['Telegram']['phone']
+username = config['Telegram']['username']
+
+# Create the client and connect
+client = TelegramClient(username, api_id, api_hash)
+
+async def main(phone):
+    await client.start()
+    print("Client Created")
+    # Ensure you're authorized
+    if await client.is_user_authorized() == False:
+        await client.send_code_request(phone)
+        try:
+            await client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+
+    me = await client.get_me()
+
+    user_input_channel = input('enter entity(telegram URL or entity id):')
+
+    if user_input_channel.isdigit():
+        entity = PeerChannel(int(user_input_channel))
+    else:
+        entity = user_input_channel
+
+    my_channel = await client.get_entity(entity)
+
+    all_messages = []
+    total_messages = 0
+    total_count_limit = 0
+
+    async for event in client.iter_admin_log(my_channel):
+        if event.deleted_message:
+            print("Dumping message", total_count_limit, "(", event.old.id, event.old.date,")")
+            all_messages.append(event.old.to_json() + ",") 
+            total_count_limit+=1
+            if event.old.media:
+                total_messages+=1
+                client.download_media(event.old.media, str(event.old.id))
+                print(" Dumped media", total_messages)
+            time.sleep(0.1)
+
+    with open('deleted_messages.json', 'w') as outfile:
+        json.dump(all_messages, outfile, cls=DateTimeEncoder)
+
+with client:
+    client.loop.run_until_complete(main(phone))
